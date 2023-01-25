474000dab30eb0c734c34ae9ebec3d19d1d3f3a8
victor.valle <victorvcunha@hotmail.com>
2023-01-25 11:03:11 -0300

    add script to generate

--- generate.sh
+++ generate.sh
@@ -1,6 +1,1 @@
-
-minica -ca-cert sphincs1.pebble.minica.pem \
--ca-key sphincs1.pebble.minica.key \
--ip-addresses 10.180.0.2 \
--ca-sig sphincs+-SHAKE256-256s-simple \
--pebble-sig Falcon-512
+go run main.go -ca-cert minica.pebble.pem -ca-key minica.pebble.key -ip-addresses 10.180.0.2 -ca-sig Falcon-512 -pebble-sig Dilithium2
